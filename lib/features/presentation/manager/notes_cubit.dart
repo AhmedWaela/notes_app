94c112a00648eae7ad0ead3c8aad39dfde1dfaa5
ahmedwael <ahmedwael99104@gmail.com>
2024-12-24 18:37:03 +0200

    create fetch notes method

--- lib/features/presentation/manager/notes_cubit.dart
+++ lib/features/presentation/manager/notes_cubit.dart
@@ -1,24 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:notes_app/features/data/models/note_model.dart';
 
 class NotesCubit extends Cubit<NoteState> {
   NotesCubit() : super(NoteInitial());
+
+  fetchAllNotes() {
+    emit(NoteLoading());
+    try {
+      var note = Hive.box<NoteModel>("Notes Box");
+      ;
+
+      var list = note.values.toList();
+      emit(NoteSuccess(notes: list));
+    } on Exception catch (e) {
+      emit(NoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
 
 sealed class NoteState {}
 
 class NoteInitial extends NoteState {}
 
-class AddNoteSuccess extends NoteState {
+class NoteSuccess extends NoteState {
   final List<NoteModel> notes;
 
-  AddNoteSuccess({required this.notes});
+  NoteSuccess({required this.notes});
 }
 
-class AddNoteLoading extends NoteState {}
+class NoteLoading extends NoteState {}
 
-class AddNoteFailure extends NoteState {
+class NoteFailure extends NoteState {
   final String errorMessage;
 
-  AddNoteFailure({required this.errorMessage});
+  NoteFailure({required this.errorMessage});
 }
